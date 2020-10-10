4e44587cffa16ecba1acc17fc95f9ecdf860bd03
Sandy Ruby <thecodingfiles@gmail.com>
2020-10-10 21:37:37 +0100

    set question array, set variables and commented on intial functions required

---
+++ quiz.js
@@ -0,0 +1,60 @@
+// list of all questions, choices, and answers
+var questions = [
+    {
+      question: "Commonly used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
+    },
+    {
+      question: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses"
+    },
+    {
+      question: "Arrays in JavaScript can be used to store ____.",
+      choices: [
+        "numbers and strings",
+        "other arrays",
+        "booleans",
+        "all of the above"
+      ],
+      answer: "all of the above"
+    },
+    {
+      question:
+        "String values must be enclosed within ____ when being assigned to variables.",
+      choices: ["commas", "curly brackets", "quotes", "parentheses"],
+      answer: "quotes"
+    },
+    {
+      question:
+        "A very useful tool used during development and debugging for printing content to the debugger is:",
+      choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
+      answer: "console.log"
+    }
+  ];
+
+    let startPage = document.getElementById('startContainer');
+    let questionPage = document.getElementById('questionContainer');
+    let gameOverPage = document.getElementById('gameOverContainer');
+
+    let timer = document.getElementById('timerDisplay');
+    let userScore = document.getElementById('scoreOut');
+    const userName = document.getElementById('userName');
+    let outcomeDisplay = document.getElementById('displayQuestionOutcome');
+    
+    const startBtn = document.getElementById('startBtn')
+    let userChoice1Btn = document.getElementById('choice1');
+    let userChoice2Btn = document.getElementById('choice2');
+    let userChoice3Btn = document.getElementById('choice3');
+    let userChoice4Btn = document.getElementById('choice4');
+    const saveScoreBtn = document.getElementById('saveScore');
+
+    // -------------functions ------------
+    // startGame
+    // setTimer
+    // endGame
+    // getQuestions
+    // checkAnswers
+
+
