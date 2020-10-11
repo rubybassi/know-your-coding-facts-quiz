a2c3fc005293c27c7cacd973bdb2b085c207013d
Sandy Ruby <thecodingfiles@gmail.com>
2020-10-11 01:12:21 +0100

    created showQuestions, startGame and checkAnswer functions

--- quiz.js
+++ quiz.js
@@ -1,11 +1,6 @@
 // list of all questions, choices, and answers
 var questions = [
     {
-      question: "Commonly used data types DO NOT include:",
-      choices: ["strings", "booleans", "alerts", "numbers"],
-      answer: "alerts"
-    },
-    {
       question: "The condition in an if / else statement is enclosed within ____.",
       choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
       answer: "parentheses"
@@ -41,20 +36,60 @@ var questions = [
     let timer = document.getElementById('timerDisplay');
     let userScore = document.getElementById('scoreOut');
     const userName = document.getElementById('userName');
+    let questionEl = document.getElementById('question');
     let outcomeDisplay = document.getElementById('displayQuestionOutcome');
     
-    const startBtn = document.getElementById('startBtn')
     let userChoice1Btn = document.getElementById('choice1');
     let userChoice2Btn = document.getElementById('choice2');
     let userChoice3Btn = document.getElementById('choice3');
     let userChoice4Btn = document.getElementById('choice4');
     const saveScoreBtn = document.getElementById('saveScore');
+    let currentQuestionIndex = 2;
 
     // -------------functions ------------
     // startGame
     // setTimer
     // endGame
-    // getQuestions
+    // ShowQuestions
     // checkAnswers
 
+    const startBtn = document.getElementById('startBtn');
+    startBtn.addEventListener('click', startGame) 
+    
+    function startGame() {
+       // console.log('started');
+       startPage.style.display = 'none';  //hide start page
+       questionPage.style.display = 'block'; //display game page
+      // currentQuestionIndex = 0;
+       showQuestion(currentQuestionIndex);
+      // console.log(questions[currentQuestionIndex])
 
+    }
+
+
+    // sets questions and answers based on currentQuesitonIndex
+    function showQuestion(q) {
+       questionEl.innerHTML = questions[q].question;
+       userChoice1Btn.textContent = questions[q].choices[0];
+       userChoice2Btn.textContent = questions[q].choices[1];
+       userChoice3Btn.textContent = questions[q].choices[2];
+       userChoice4Btn.textContent = questions[q].choices[3];
+    }
+
+        
+    
+    // checks answer using jQuery and event delagation
+        $("#choiceBtns button").on("click", (function(event) {
+	
+		var userGuess = $(this).text();
+		if (userGuess === questions[currentQuestionIndex].answer) {
+            console.log("correct")
+		//	clearInterval(clock);
+		//	userWin();
+		}
+		else {
+            console.log("incorrect")
+		//	clearInterval(clock);
+		//	userLoss();
+		}
+	}));
